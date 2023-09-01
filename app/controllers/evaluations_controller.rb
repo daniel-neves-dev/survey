d229a619b74cb5981015ce6ac8d6f6d53668ca14
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2023-09-01 09:28:44 -0300

    feat: evaluation index

--- app/controllers/evaluations_controller.rb
+++ app/controllers/evaluations_controller.rb
@@ -63,7 +63,7 @@ class EvaluationsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_evaluation
-      @evaluation = Evaluation.find(params[:id])
+      @evaluation = Section.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
