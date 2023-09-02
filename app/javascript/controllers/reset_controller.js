80c3654f3a678fb11767662caa6ef0cafab5a737
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2023-09-02 11:47:09 -0300

    feat:questions

--- app/javascript/controllers/reset_controller.js
+++ app/javascript/controllers/reset_controller.js
@@ -5,4 +5,11 @@ export default class extends Controller {
   resetForm() {
     document.getElementById('modal').remove()
   }
+
+  questionForm(){
+    const timer = setInterval(() =>{
+      document.getElementById('new_question').reset()
+      clearInterval(timer)
+    },100)
+  }
 }
